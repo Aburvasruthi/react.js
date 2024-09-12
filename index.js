b7b0a074cef76c6cad1fdcef97a881c7e632c142
Aburvasruthi <147430112+Aburvasruthi@users.noreply.github.com>
2024-09-12 10:49:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Functional Component
+function Greeting() {
+  return <h1>Hello, World!</h1>;
+}
+
+// Class Component
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hello, World!</h1>;
+  }
+}
+
